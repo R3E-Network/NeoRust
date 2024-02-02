68248cb942e8668f0661466ea3640041d1ab5d9f
IssacLi <1498262377@qq.com>
2024-02-02 14:00:45 +0800

    binary test

--- neo-codec/src/binary_encoder.rs
+++ neo-codec/src/binary_encoder.rs
@@ -1,4 +1,5 @@
 use crate::{encode::NeoSerializable, CodecError};
+use num_bigint::ToBigInt;
 /// A binary encoder that can write various primitive types and serializable objects to a byte vector.
 ///
 /// # Examples
@@ -48,7 +49,7 @@ impl Encoder {
 	}
 
 	pub fn write_i64(&mut self, v: i64) {
-		self.write_u64(v as u64);
+		self.data.extend_from_slice(&v.to_le_bytes());
 	}
 
 	pub fn write_u16(&mut self, v: u16) {
@@ -63,18 +64,21 @@ impl Encoder {
 		self.data.extend_from_slice(bytes);
 	}
 
-	// Other primitive write methods
-	pub fn write_var_int(&mut self, value: i64) {
-		match value {
-			0..=0xfd => self.write_u8(value as u8),
-			0x10000..=0xffffffff => {
-				self.write_u8(0xfd);
-				self.write_u16(value as u16);
-			},
-			_ => {
-				self.write_u8(0xff);
-				self.write_u64(value as u64);
-			},
+	fn write_var_int(&mut self, v: i64) {
+		if (v < 0) {
+			panic!("Negative value not allowed")
+		}
+		if v < 0xfd {
+			self.write_u8(v as u8)
+		} else if v <= u16::MAX as i64 {
+			self.write_u8(0xfd);
+			self.write_u16(v as u16);
+		} else if v <= u32::MAX as i64 {
+			self.write_u8(0xfe);
+			self.write_u32(v as u32);
+		} else {
+			self.write_u8(0xff);
+			self.write_i64(v);
 		}
 	}
 
@@ -140,5 +144,128 @@ impl Hasher for Encoder {
 
 	fn write(&mut self, bytes: &[u8]) {
 		self.write_bytes(bytes);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::Encoder;
+
+	#[test]
+	fn test_write_u32() {
+		let mut writer = Encoder::new();
+
+		let max = u32::MAX;
+		writer.write_u32(max);
+		assert_eq!(writer.to_bytes(), vec![0xff; 4]);
+		writer.reset();
+		writer.write_u32(0);
+		assert_eq!(writer.to_bytes(), vec![0; 4]);
+		writer.reset();
+		writer.write_u32(12345);
+		assert_eq!(writer.to_bytes(), vec![0x39, 0x30, 0, 0]);
+	}
+
+	#[test]
+	fn test_write_i64() {
+		let mut writer = Encoder::new();
+
+		writer.write_i64(0x1234567890123456i64);
+		assert_eq!(writer.to_bytes(), [0x56, 0x34, 0x12, 0x90, 0x78, 0x56, 0x34, 0x12]);
+
+		writer.reset();
+		writer.write_i64(i64::MAX);
+		assert_eq!(writer.to_bytes(), [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f]);
+
+		writer.reset();
+		writer.write_i64(i64::MIN);
+		assert_eq!(writer.to_bytes(), [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80]);
+
+		writer.reset();
+		writer.write_i64(0);
+		assert_eq!(writer.to_bytes(), vec![0u8; 8]);
+
+		writer.reset();
+		writer.write_i64(1234567890);
+		assert_eq!(writer.to_bytes(), vec![0xd2, 0x02, 0x96, 0x49, 0, 0, 0, 0]);
+	}
+
+	#[test]
+	fn test_write_u16() {
+		let mut writer = Encoder::new();
+
+		let max = u16::MAX;
+		writer.write_u16(max);
+		assert_eq!(writer.to_bytes(), vec![0xff; 2]);
+
+		writer.reset();
+		writer.write_u16(0);
+		assert_eq!(writer.to_bytes(), vec![0; 2]);
+
+		writer.reset();
+		writer.write_u16(12345);
+		assert_eq!(writer.to_bytes(), vec![0x39, 0x30]);
+	}
+
+	#[test]
+	fn test_write_var_int() {
+		let mut writer = Encoder::new();
+
+		writer.write_var_int(0);
+		assert_eq!(writer.to_bytes(), vec![0]);
+
+		writer.reset();
+		writer.write_var_int(252);
+		assert_eq!(writer.to_bytes(), vec![0xfc]);
+
+		writer.reset();
+		writer.write_var_int(253);
+		assert_eq!(writer.to_bytes(), vec![0xfd, 0xfd, 0]);
+
+		writer.reset();
+		writer.write_var_int(65_534);
+		assert_eq!(writer.to_bytes(), vec![0xfd, 0xfe, 0xff]);
+
+		writer.reset();
+		writer.write_var_int(65_536);
+		assert_eq!(writer.to_bytes(), vec![0xfe, 0, 0, 1, 0]);
+
+		writer.reset();
+		writer.write_var_int(4_294_967_295);
+		assert_eq!(writer.to_bytes(), vec![0xfe, 0xff, 0xff, 0xff, 0xff]);
+
+		writer.reset();
+		writer.write_var_int(4_294_967_296);
+		assert_eq!(writer.to_bytes(), vec![0xff, 0, 0, 0, 0, 1, 0, 0, 0]);
+	}
+
+	#[test]
+	fn test_write_var_bytes() {
+		let mut writer = Encoder::new();
+
+		let bytes = hex::decode("010203").unwrap();
+		writer.write_var_bytes(&bytes);
+		assert_eq!(writer.to_bytes(), hex::decode("03010203").unwrap());
+
+		writer.reset();
+		let bytes = "00102030102030102030102030102030102030102030102030102030102030102031020301020301020301020301020301020301020301020301020301020301020310203010203010203010203010203010203010203010203010203010203010203102030102030102030102030102030102030102030102030102030102030102030010203010203010203010203010203010203010203010203010203010203010203102030102030102030102030102030102030102030102030102030102030102031020301020301020301020301020301020301020301020301020301020301020310203010203010203010203010203010203010203010203010203010203010203";
+		writer.write_var_bytes(&hex::decode(bytes.clone()).unwrap());
+		assert_eq!(writer.to_bytes(), hex::decode(format!("fd0601{}", bytes)).unwrap());
+	}
+
+	#[test]
+	fn test_write_var_string() {
+		let mut writer = Encoder::new();
+
+		let s = "hello, world!";
+		writer.write_var_string(s);
+		assert_eq!(writer.to_bytes(), hex::decode("0d68656c6c6f2c20776f726c6421").unwrap());
+		writer.reset();
+		let s = "hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!hello, world!";
+		writer.write_var_string(&s);
+		assert_eq!(
+			writer.to_bytes(),
+			[hex::decode("fd1502").unwrap(), s.as_bytes().to_vec()].concat()
+		);
 	}
 }
