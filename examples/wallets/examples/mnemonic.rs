3ee9eca2c548c622c42b9a4bf1e7354f88be5b34
Jimmy <jinghui@wayne.edu>
2024-11-22 00:08:36 +0800

    update example

--- examples/wallets/examples/mnemonic.rs
+++ examples/wallets/examples/mnemonic.rs
@@ -2,32 +2,21 @@ use eyre::Result;
 use NeoRust::prelude::*;
 
 fn main() -> Result<()> {
-    let phrase = "work man father plunge mystery proud hollow address reunion sauce theory bonus";
-    let index = 0u32;
-    let password = "TREZOR123";
+    // Create a new BIP39 account with password
+    let password = "my secure password";
+    let account = Bip39Account::create(password)?;
+    
+    println!("New Account:");
+    println!("Address: {}", account.account.get_address());
+    println!("Mnemonic: {}", account.mnemonic);
 
-    // Access mnemonic phrase with password
-    // Child key at derivation path: m/44'/60'/0'/0/{index}
-    let wallet = MnemonicBuilder::<English>::default()
-        .phrase(phrase)
-        .index(index)?
-        // Use this if your mnemonic is encrypted
-        .password(password)
-        .build()?;
+    // Recover an existing account from mnemonic
+    let mnemonic = "work man father plunge mystery proud hollow address reunion sauce theory bonus";
+    let recovered = Bip39Account::from_bip39_mnemonic(password, mnemonic)?;
 
-    eprintln!("Wallet: {wallet:?}");
-
-    // Generate a random wallet (24 word phrase) at custom derivation path
-    let mut rng = rand::thread_rng();
-    let wallet = MnemonicBuilder::<English>::default()
-        .word_count(24)
-        .derivation_path("m/44'/60'/0'/2/1")?
-        // Optionally add this if you want the generated mnemonic to be written
-        // to a file
-        // .write_to(path)
-        .build_random(&mut rng)?;
-
-    eprintln!("Random wallet: {wallet:?}");
+    println!("\nRecovered Account:");
+    println!("Address: {}", recovered.account.get_address());
+    println!("Mnemonic: {}", recovered.mnemonic);
 
     Ok(())
 }
